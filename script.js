751dbefb1dbf58a554cf87d7f0b4edd4982f62e3
Dennis <121116755+denyo177@users.noreply.github.com>
2023-01-27 11:53:03 +0100

    Create function game() and finished first version

--- script.js
+++ script.js
@@ -12,24 +12,38 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-    playerSelection = capitalizeString(playerSelection);
     // Compare player- and computer selection and define winner
     if (playerSelection === computerSelection) {
-        return "Draw!"
+        alert("Draw!");
+        return 0; 
 
     } else if (playerSelection === "Paper") {
-        if (computerSelection === "Scissors") return "You Lose! Scissors beats Paper";
-        else if (computerSelection === "Rock") return "You Win! Paper beats Rock";
+        if (computerSelection === "Scissors") {
+            alert("You Lose! Scissors beats Paper"); 
+            return 1; // Comp wins
+        } else if (computerSelection === "Rock") {
+            alert("You Win! Paper beats Rock");
+            return 2; // Player wins
+        }
 
     } else if (playerSelection === "Scissors") {
-        if (computerSelection === "Paper") return "You Win! Scissors beats Paper";
-        else if (computerSelection === "Rock") return "You Lose! Rock beats Scissors"
+        if (computerSelection === "Paper") {
+            alert("You Win! Scissors beats Paper");
+            return 2; //Player wins
+        } else if (computerSelection === "Rock") {
+            alert("You Lose! Rock beats Scissors");
+            return 1; //Comp Wins
+        }
 
     } else if (playerSelection === "Rock") {
-        if (computerSelection === "Paper") return "You Lose! Paper beats Rock";
-        else if (computerSelection === "Scissors") return "You Win! Rock beats Scissors";
-
-    } else return "Invalid Selection! Please Enter either 'Paper', 'Scissors' or 'Rock'";
+        if (computerSelection === "Paper") {
+            alert("You Lose! Paper beats Rock");
+            return 1; //Comp Wins
+        } else if (computerSelection === "Scissors") {
+            alert("You Win! Rock beats Scissors");
+            return 2; // Player Wins
+        }
+    }
 
 }
 
@@ -37,6 +51,34 @@ function capitalizeString (word) {
     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+
+    let winsPlayer = 0;
+    let winsComputer = 0;
+
+    for  (let i = 0; i < 5; i++) {
+        // Abfrage Players Choice. Generieren Computer Choice
+        let playerSelection = prompt("Enter your choice of 'Paper', 'Scissors' or 'Rock'! Round " + (i+1) + "/5");
+        playerSelection = capitalizeString(playerSelection);
+        computerSelection = getComputerChoice();
+
+        let resultRound = 0;
+        // Spiele Runde, speicher Ergebnis je Spieler. 
+        resultRound = playRound(playerSelection, computerSelection);
+        if (resultRound === 1) {
+            winsComputer += 1;
+        } else if (resultRound === 2) {
+            winsPlayer += 1;
+        }
+    }
+    // Nach 5 Runden, gebe Gewinner aus
+    if (winsPlayer > winsComputer) {
+        alert(`Game Over. Congratulation, You Won the game! [You: ${winsPlayer} - Computer: ${winsComputer}]`);
+    } else if (winsComputer > winsPlayer) {
+        alert(`Game Over. Don't be sad, You Lost the Game :( ! [You: ${winsPlayer} - Computer: ${winsComputer}]`);
+    } else alert(`Game Over. Draw! [You: ${winsPlayer} - Computer: ${winsComputer}]`)
+}
